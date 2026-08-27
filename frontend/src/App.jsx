7afabdda38bcf4fb2cf784a372c2402e134afa02
pari Vaish <pariv7006@gmail.com>
2026-08-27 12:02:38 +0530

    fix: correct case-sensitive import paths for Render build

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Header from "./components/Header";
-import Login from "./pages/login";
+import Login from "./pages/Login";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import AdminLayout from "./layouts/AdminLayout";
